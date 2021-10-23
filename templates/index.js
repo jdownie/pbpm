8b4f479454c65972a6d00b0fe1343d09336e4b26
James Downie <jdownie@gmail.com>
2021-10-23 21:57:37 +1000

    Cleaned up a bunch of javascript warnings.

--- templates/index.js
+++ templates/index.js
@@ -1,23 +1,23 @@
 var pbpmApp = new Vue(
-{ el: '#pbpm'
-, delimiters: ['[[',']]']
-, data() {
+{ el: '#pbpm',
+  delimiters: ['[[',']]'],
+  data: function() {
     return {
       'tab': null,
       'form': null,
       'landscape': null,
       'cfg': { 'table':
-               { 'station': { 'label': 'Stations', 'fields': { 'name': 'Name' } }
-               , 'service': { 'label': 'Services', 'fields': { 'url_template': 'URL Template' } }
-               , 'router': { 'label': 'Routers', 'fields': { 'description': 'Description' } }
-               , 'owner': { 'label': 'Owners', 'fields': { 'name': 'Name' } }
-               , 'map': { 'label': 'Maps', 'fields': { 'name': 'Name' } }
-               }
-             , 'debug': false
+               { 'station': { 'label': 'Stations', 'fields': { 'name': 'Name' } },
+                 'service': { 'label': 'Services', 'fields': { 'url_template': 'URL Template' } },
+                 'router': { 'label': 'Routers', 'fields': { 'description': 'Description' } },
+                 'owner': { 'label': 'Owners', 'fields': { 'name': 'Name' } },
+                 'map': { 'label': 'Maps', 'fields': { 'name': 'Name' } }
+               },
+               'debug': false
              }
-    }
-  }
-, mounted() {
+    };
+  },
+  mounted: function() {
     this.load();
     $('#pbpm').fadeIn();
     window.addEventListener("dblclick", function(event) { pbpmApp.cfg.debug = ! pbpmApp.cfg.debug; }, false);
@@ -27,36 +27,36 @@ var pbpmApp = new Vue(
       pbpmApp.editMap('AUC');
     }, 500);
     */
-  }
-, filters: {
-  configStepLabel: function(landscape, record, map_code, i) {
-    var type = record.type.toUpperCase();
-    var ret = ( i + 1 ) + ') ' + type + ':' + record.code;
-    var label = '';
-    if (type == 'STATION') {
-      if ( landscape.station[record.code] != undefined) {
-        label = ' - ' + landscape.station[record.code].name;
+  },
+  filters: {
+    configStepLabel: function(landscape, record, map_code, i) {
+      var type = record.type.toUpperCase();
+      var ret = ( i + 1 ) + ') ' + type + ':' + record.code;
+      var label = '';
+      if (type == 'STATION') {
+        if ( landscape.station[record.code] != undefined) {
+          label = ' - ' + landscape.station[record.code].name;
+        }
       }
+      ret = ret + label;
+      return ret;
     }
-    ret = ret + label;
-    return ret;
-  }
-}
-, methods: {
+  },
+  methods: {
     drawMapNode: function() {
       var w = $('#diagram').width();
       $('#diagram').html('<canvas id="diagramCanvas"></canvas>');
       $('#diagramCanvas').css('width', w + 'px').css('height', w / 2 + 'px');
-      const canvas = document.getElementById('diagramCanvas');
-      const ctx = canvas.getContext('2d');
+      var canvas = document.getElementById('diagramCanvas');
+      var ctx = canvas.getContext('2d');
       ctx.fillStyle = 'green';
       ctx.fillRect(10, 10, 10, 10);
     },
-    mapAddStation(e) {
+    mapAddStation: function(e) {
       if (this.form.add.station_code != '') {
         var record = { 'type': 'station', 'code': this.form.add.station_code };
         var srcActions = this.landscape.station[this.form.add.station_code].actions;
-        var dstActions = new Array();
+        var dstActions = [];
         for (var a in srcActions) {
           var newAction = { 'code': srcActions[a].code, 'leads_to': null };
           dstActions.push(newAction);
@@ -67,7 +67,7 @@ var pbpmApp = new Vue(
         this.save();
       }
     },
-    mapAddService(e) {
+    mapAddService: function(e) {
       if (this.form.add.service_code != '') {
         var record = { 'type': 'service', 'code': this.form.add.service_code };
         this.landscape.map[this.form.code].config.push(record);
@@ -75,11 +75,11 @@ var pbpmApp = new Vue(
         this.save();
       }
     },
-    mapAddRouter(e) {
+    mapAddRouter: function(e) {
       if (this.form.add.router_code != '') {
         var record = { 'type': 'router', 'code': this.form.add.router_code };
         var srcActions = this.landscape.router[this.form.add.router_code].actions;
-        var dstActions = new Array();
+        var dstActions = [];
         for (var a in srcActions) {
           var newAction = { 'code': srcActions[a].action_code, 'leads_to': null };
           dstActions.push(newAction);
@@ -90,7 +90,7 @@ var pbpmApp = new Vue(
         this.save();
       }
     },
-    mapSelect(config_id, action_id = undefined) {
+    mapSelect: function(config_id, action_id) {
       // First, check to see if we're selecting a station or not...
       var type = this.landscape.map[this.form.code].config[config_id].type;
       var code = this.landscape.map[this.form.code].config[config_id].code;
@@ -102,8 +102,6 @@ var pbpmApp = new Vue(
           this.form.leads_to = '';
         } else {
           this.form.edit = [ config_id, action_id ];
-          var type = this.landscape.map[this.form.code].config[config_id].type;
-          var code = this.landscape.map[this.form.code].config[config_id].code;
           var action = this.landscape.map[this.form.code].config[config_id].actions[action_id];
           if (action.leads_to != undefined && action.leads_to != null) {
             this.form.leads_to = action.leads_to;
@@ -112,7 +110,7 @@ var pbpmApp = new Vue(
           }
         }
       } else if (type == 'station' && code == 'BEGIN' || type == 'service') {
-        console.log('Handling as a service or BEGIN selection.')
+        console.log('Handling as a service or BEGIN selection.');
         if (this.form.edit != null && this.form.edit[0] == config_id) {
           this.form.edit = null;
           this.form.leads_to = '';
@@ -150,16 +148,16 @@ var pbpmApp = new Vue(
         for (var field in fields) {
           record[field] = this.form[field];
         }
-        this.landscape[item][this.form['code']] = record;
+        this.landscape[item][this.form.code] = record;
       }
       this.save();
       this.resetForm();
     },
-    editItemBegin(item, code) {
-      this.form['edit_code'] = code;
+    editItemBegin: function(item, code) {
+      this.form.edit_code = code;
       this.vueRender();
     },
-    editItemEnd(item) {
+    editItemEnd: function(item) {
       delete this.form.edit_code;
       this.vueRender();
       this.save();
@@ -172,7 +170,7 @@ var pbpmApp = new Vue(
     linkMap: function() {
       var src = this.form.edit;
       var dst = this.form.leads_to;
-      var node = this.landscape.map[this.form.code].config[src[0]]
+      var node = this.landscape.map[this.form.code].config[src[0]];
       if (src.length == 2) {
         node = node.actions[src[1]];
       }
@@ -185,7 +183,7 @@ var pbpmApp = new Vue(
           this.landscape.station[code].actions = [];
         }
         this.selectTab('editStation');
-        this.form = { 'code': code, 'add': { 'action_code': '', 'label': '', 'color': '#198754' }, 'edit': null }
+        this.form = { 'code': code, 'add': { 'action_code': '', 'label': '', 'color': '#198754' }, 'edit': null };
       }
     },
     editRouter: function(code) {
@@ -194,27 +192,27 @@ var pbpmApp = new Vue(
           this.landscape.station[code].actions = [];
         }
         this.selectTab('editRouter');
-        this.form = { 'code': code, 'add': { 'action_code': '', 'label': '', 'expression': '' }, 'edit': null }
+        this.form = { 'code': code, 'add': { 'action_code': '', 'label': '', 'expression': '' }, 'edit': null };
       }
     },
-    addStationAction() {
+    addStationAction: function() {
       var record = JSON.parse(JSON.stringify(this.form.add));
       this.landscape.station[this.form.code].actions.push(record);
       this.editStation(this.form.code);
       this.save();
     },
-    addRouterAction() {
+    addRouterAction: function() {
       var record = JSON.parse(JSON.stringify(this.form.add));
       this.landscape.router[this.form.code].actions.push(record);
       this.editRouter(this.form.code);
       this.save();
     },
-    delStationAction(i) {
+    delStationAction: function(i) {
       console.log('dsa', i);
       this.landscape.station[this.form.code].actions.splice(i, 1);
       this.save();
     },
-    delRouterAction(i) {
+    delRouterAction: function(i) {
       console.log('dra', i);
       this.landscape.router[this.form.code].actions.splice(i, 1);
       this.save();
@@ -227,7 +225,7 @@ var pbpmApp = new Vue(
       var map = this.cfg.table;
       if (map[this.tab] != undefined) {
         var record = {};
-        record['code'] = '';
+        record.code = '';
         var fields = map[this.tab].fields;
         for (var field in fields) {
           record[field] = '';
